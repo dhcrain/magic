320f15e9314de8e71103be91eb53d3563452aaec
Davis Crain <davis.crain@onepeloton.com>
2023-08-25 12:56:34 -0400

    ADD-MODELS CRM models contact, company, person

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+# import modeLs from crm app
+from .models import ContactInfo, Company, Person
 
-# Register your models here.
+
+# Register ALL IMPORTED MODELS
+admin.site.register(ContactInfo)
+admin.site.register(Company)
+admin.site.register(Person)
+
